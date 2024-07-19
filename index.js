ea25d80d69ecc6327a538fcd087aae8f3eb9b4d3
Vilmar Cabanero <vilmarcabanero@gmail.com>
2024-07-19 17:29:46 +0800

    Try gitlab ci for master on push

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.json({ message: "Practice CI CD app is running." });
+  res.json({ message: "Practice CI CD app is running.", update: "Update 1 - try gitlab ci for master on push"});
 });
 
 app.listen(port, () => {
